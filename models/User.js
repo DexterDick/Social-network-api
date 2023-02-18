51a8598720d241a48b14d1ed89d860e01fdad526
Dexter Dick <@dexterdick@hotmail.com>
2023-02-18 18:45:46 -0500

    added route api and user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+// schema to create user model
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+// create user monel from userSchema
+
+const User = model("user", userSchema);
+
+// export user
+model.exports = User;
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { getThought } = require("../../controllers/thoughtController.js");
+
+// api/thought
+router.route("/");
+
+module.exports = router;
