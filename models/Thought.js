4e07524e12980633ea8d573b5e62d01b43f7962f
Dexter Dick <@dexterdick@hotmail.com>
2023-02-18 19:23:05 -0500

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+// create thought schema
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+// create model using thoughtSchema
+const Thought = model("thought", thoughtSchema);
+
+// export Thought model
+
+module.exports = Thought;
