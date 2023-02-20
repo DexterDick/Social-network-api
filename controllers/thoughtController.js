d6c062c0b4a67b1e71d66786ca2dc2c7490e9e42
Dexter Dick <@dexterdick@hotmail.com>
2023-02-20 15:50:18 -0500

    made updates to thought controller to update by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,8 +1,24 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-    getAllThoughts(req, res) {},
-    getSingleThoughtById(req, res) {},
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then((thoughtData) => res.status(200).json(thoughtData))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    getSingleThoughtById(req, res) {
+        Thought.findOne({ _id: req.params.id })
+            .then((thoughtData) => {
+                res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
     createThought(req, res) {
         Thought.create(req.body)
             .then(({ _id }) => {
@@ -23,7 +39,24 @@ module.exports = {
                 return res.status(500).json(err);
             });
     },
-    updateThoughtById(req, res) {},
+    updateThoughtById(req, res) {
+        Thought.updateOneandUdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thoughtData) => {
+                !thoughtData
+                    ? res
+                          .status(404)
+                          .json({ message: "No Thought with this id!" })
+                    : res.status(200).json(thoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
     deleteThoughtById(req, res) {},
     createReaction(req, res) {},
     deleteReactionById(req, res) {},
