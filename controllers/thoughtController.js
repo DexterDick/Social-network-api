d7294daef8e8d84c7528c49a6c40de07301f9165
Dexter Dick <@dexterdick@hotmail.com>
2023-02-20 12:46:03 -0500

    updated thoughts to save id to user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,26 @@ const { User, Thought } = require("../models");
 module.exports = {
     getAllThoughts(req, res) {},
     getSingleThoughtById(req, res) {},
-    createThought(req, res) {},
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then(({ _id }) => {
+                console.log(_id);
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then((thoughtData) => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: "no user found with id" });
+                }
+                res.json(thoughtData);
+            })
+            .catch((err) => {
+                return res.status(500).json(err);
+            });
+    },
     updateThoughtById(req, res) {},
     deleteThoughtById(req, res) {},
     createReaction(req, res) {},
--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,21 @@ module.exports = {
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
-    updateUser(req, res) {},
-    deleteUser(req, res) {},
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with this id!" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    deleteUser(req, res) {
+        User.deleteOne({ _id: req.params.id });
+    },
 };
